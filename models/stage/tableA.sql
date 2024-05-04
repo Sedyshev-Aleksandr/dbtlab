64eb44994ab4850f714fe3cd57085760c0b2e164
Oleksandr Siedyshev <sed_281521@protonmail.com>
2024-05-04 21:32:39 +0000

    delete column

--- models/stage/tableA.sql
+++ models/stage/tableA.sql
@@ -1,9 +1,9 @@
-select 'TEST RPOD A' as first_name, 'email1' as email, 'address1' as address 
+select  'email1' as email, 'address1' as address 
 union all
-select 'TEST PROD B' as first_name, 'email2' as email, 'address2' as address 
+select  'email2' as email, 'address2' as address 
 union all
-select 'Petya' as first_name, 'email3' as email, 'address3' as address 
+select  'email3' as email, 'address3' as address 
 union all
-select 'Sveta' as first_name, 'email4' as email, 'address4' as address 
+select  'email4' as email, 'address4' as address 
 union all
-select 'Olya' as first_name, 'email5' as email, 'address5' as address 
+select  'email5' as email, 'address5' as address 
