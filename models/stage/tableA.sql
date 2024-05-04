a254bcee5f39b4e7d72dca37b77cb833a3b46805
Oleksandr Siedyshev <sed_281521@protonmail.com>
2024-05-04 18:14:51 +0000

    deleted column

--- models/stage/tableA.sql
+++ models/stage/tableA.sql
@@ -1,9 +1,9 @@
-select 'TEST RPOD A' as first_name, 'email1' as email
+select 'TEST RPOD A' as first_name
 union all
-select 'TEST PROD B' as first_name, 'email2' as email
+select 'TEST PROD B' as first_name
 union all
-select 'Petya' as first_name, 'email3' as email
+select 'Petya' as first_name
 union all
-select 'Sveta' as first_name, 'email4' as email
+select 'Sveta' as first_name
 union all
-select 'Olya' as first_name, 'email5' as email
+select 'Olya' as first_name
