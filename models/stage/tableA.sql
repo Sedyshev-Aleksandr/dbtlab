35c3f4278ae68a83dc95364f17330cfa8f07d6c3
Oleksandr Siedyshev <sed_281521@protonmail.com>
2024-05-04 21:17:00 +0000

    address

--- models/stage/tableA.sql
+++ models/stage/tableA.sql
@@ -1,9 +1,9 @@
-select 'TEST RPOD A' as first_name, 'email1' as email, 'address1' as address 
+select 'TEST RPOD A' as first_name, 'email1' as email
 union all
-select 'TEST PROD B' as first_name, 'email2' as email, 'address2' as address 
+select 'TEST PROD B' as first_name, 'email2' as email
 union all
-select 'Petya' as first_name, 'email3' as email, 'address3' as address 
+select 'Petya' as first_name, 'email3' as email
 union all
-select 'Sveta' as first_name, 'email4' as email, 'address4' as address 
+select 'Sveta' as first_name, 'email4' as email
 union all
-select 'Olya' as first_name, 'email5' as email, 'address5' as address 
+select 'Olya' as first_name, 'email5' as email 
