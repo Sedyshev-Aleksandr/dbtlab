241afa95d57effb7c4fe465c6d4a80ce73270f2f
Oleksandr Siedyshev <sed_281521@protonmail.com>
2024-05-05 20:42:37 +0000

    add macros

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,11 +1,12 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
+
     {%- if custom_schema_name is none -%}
 
-        {{ default_schema }}
-        
-    {%- elif target.name == "prod" -%}
+        {{ default_schema }}  
+
+    {%- elif var('database_name',target.name) == "prod" -%}
 
         {{ custom_schema_name | trim }}       
 
