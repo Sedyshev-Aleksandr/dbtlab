0320c13cee92031050aec965e3cbbea743ece0b0
Oleksandr Siedyshev <sed_281521@protonmail.com>
2024-05-06 12:12:41 +0000

    new column new phone

--- models/dwh/Person_dim.sql
+++ models/dwh/Person_dim.sql
@@ -1,5 +1,6 @@
 select concat('Hello ',t.full_name) as my_name,
        upper(t.email) as upper_email,
        concat('I live in ', t.address) as desc_address,
-       concat('+47-', t.phone) as phone_with_code
+       concat('+47-', t.phone) as phone_with_code,
+       home_phone as my_home_number
 from {{ ref('tableA') }} t
